20f43dd98ab3a9a67054320ad5543c48dd72d6ec
kiankoba <kiankoba@users.noreply.github.com>
2017-10-10 11:43:00 -1000

    Class Finished

--- app/leaderboard.js
+++ app/leaderboard.js
@@ -1,1 +1,29 @@
-console.log("Hello world");
+PlayersList = new Mongo.Collection('players');
+
+if(Meteor.isClient){
+  console.log("Hello client");
+  Template.leaderboard.helpers({
+    'player': function(){
+      return PlayersList.find();
+    },
+    'otherHelperFunction': function(){
+      return "Some other function";
+    },
+    'selectedClass': function(){
+      return "selected";
+    }
+  });
+
+  Template.leaderboard.events({
+    'click .player': function(){
+      var playerId = this._id;
+      Session.set('selectedPlayer', playerId);
+    }
+  });
+}
+
+
+
+if(Meteor.isServer){
+  console.log("Hello server");
+}
